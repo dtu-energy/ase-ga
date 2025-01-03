313c7b964452a347b818b1cb68caa2d46d981872
Jakob Schiotz <schiotz@fysik.dtu.dk>
2025-01-03 09:56:26 +0100

    Cleanup of test code, 5% faster.

--- ase/test/md/test_langevin_offbyone.py
+++ ase/test/md/test_langevin_offbyone.py
@@ -37,8 +37,7 @@ def tempcurve(t, A, tau, B):
     return A * np.exp(-t / tau) + B
 
 
-def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None,
-            intval=5, plot=False, sloppytime=False, failfluct=False):
+def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None, intval=5):
     '''Run NVT dynamics, testing the behaviour.
 
     Parameters:
@@ -49,14 +48,9 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None,
     dynmaker:   Function making the dynamics object.
     rng:        Random number generator.
     intval:     Interval for taking data.
-    plot:       Plot the swing-in temperature graph (default: False).
-    sloppytime: Test of swing-in time is sloppy (+/- 50% instead of +/- 1%).
-                (for nondeterministic dynamics)
-    failfluct:  Test of energy fluctuations is expected to fail.
-                (for dynamics that does not produce a true Canonical Ensemble)
     '''
     runtime = nsteps * dt
-    tau = runtime / 10    # Energy relaxation time in ideal gas
+    tau = runtime / 5    # Energy relaxation time in ideal gas
 
     # We pass *half* the energy relaxation time to the generator of the
     # dynamics, as we have a solid, where the relaxation time will be twice
@@ -67,32 +61,19 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None,
     dyn.attach(measure, interval=1)
     dyn.run(nsteps)
     temperatures = measure.temperatures
-    del dyn
 
-    # Fit temperature curve
-    temperatures = temperatures[5:]
-    times = np.arange(len(temperatures)) * dt
-    (DeltaTfit, tau_fit, T_fit), _ = curve_fit(tempcurve, times, temperatures,
-                                                   (-T0, tau, T0))
-
-    # Run again with smaller tau
-    tausmall = runtime / 10
-    dyn = dynmaker(atoms, T0, dt, tausmall, rng=rng, logint=2500)
+    # Run again while taking data
+    dyn = dynmaker(atoms, T0, dt, tau, rng=rng, logint=2500)
     measure = MeasureEnergy(atoms)
     dyn.attach(measure, interval=intval)
     com_before = atoms.get_center_of_mass()
-    if failfluct:
-        # No need for good statistics if it fails anyway
-        dyn.run(nsteps)
-    else:
-        dyn.run(nsteps * 10)
+    dyn.run(nsteps * 20) # XXXX
     com_after = atoms.get_center_of_mass()
     energies = measure.energies
     energies = energies[len(energies) // 10:]
     temperatures2 = measure.temperatures
     temperatures2 = temperatures2[len(temperatures2) // 5:]
     times2 = np.arange(len(temperatures2)) * dt * intval
-    del dyn
 
     stdE = np.std(energies)
     avgT = np.mean(temperatures2)
@@ -100,45 +81,22 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None,
     expected = kB * T0 * np.sqrt(3 * len(atoms))
 
     # Output results
-    print(f'Part 1 temperature:   {T_fit:.2f} K    (expected {T0:.2f})')
-    print(f'Time constant:       {tau_fit / fs:.1f} fs  '
-              + '(expected {tau / fs:.1f}  '
-              + 'error {(tau_fit / tau - 1) * 100:.1f}%)')
-    print(f'Initial temperature offset:  {DeltaTfit:.2f} K')
-    print('Note: Due to the small system size, the numbers above will')
-    print('      be completely off.')
-    print()
     print(f'Observed energy fluctuation: {stdE:.2f} eV')
     print(f'Expected energy fluctuation: {expected:.2f} eV')
     print(f'Error: {(stdE / expected - 1) * 100:.1f}%')
-    if failfluct:
-        print('Failed fluctuations EXPECTED for this dynamics!')
-    else:
-        assert np.abs(stdE - expected) < 0.25 * expected, 'Energy fluctuations'
+    assert np.abs(stdE - expected) < 0.25 * expected, 'Energy fluctuations'
 
     # Temperature error: We should be able to detect a error of 1/N_atoms
     maxtemperr = 2 / 3 * 3 / atoms.get_number_of_degrees_of_freedom()
     # ... but not if we don't have good statistics.
-    if failfluct:
-        maxtemperr *= 3
     print(f'Observed average temperature:  {avgT:.2f} K'
-              + '   (expected {T0:.2f} K)')
+              + f'   (expected {T0:.2f} K)')
     print(f'Error: {(avgT / T0 - 1) * 100:.1f}%  '
-              + '(max: {maxtemperr * 100:.1f}%)')
+              + f'(max: {maxtemperr * 100:.1f}%)')
     assert np.abs(avgT - T0) < T0 * maxtemperr, 'Temperature'
 
     print('Center of mass before:', com_before)
     print('Center of mass after: ', com_after)
-
-    if plot:
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots()
-        ax.plot(times, temperatures, 'b.')
-        ax.plot(times, tempcurve(times, DeltaTfit, tau_fit, T_fit), 'k-')
-        fig2, ax2 = plt.subplots()
-        ax2.plot(times2, temperatures2, 'b.')
-        plt.show(block=True)
-        del fig, fig2
 
 
 def lgvdynmaker(atoms, T0, dt, tau, rng, logint):
@@ -152,9 +110,9 @@ def lgvdynmaker(atoms, T0, dt, tau, rng, logint):
 def test_langevin_offbyone(asap3):
     T0 = 300
     dt = 5 * fs
-    nsteps = 10000
+    nsteps = 5000
     rng = np.random.default_rng(2718281828459045)
 
     atoms = make_atoms(T0 / 10, rng, asap3)
     run_nvt(atoms, nsteps, dt, T0, dynmaker=lgvdynmaker,
-            rng=rng, plot=False, sloppytime=True)
+            rng=rng)
