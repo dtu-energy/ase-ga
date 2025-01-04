c63b265d405d41d38d0190f2c38104c42427ed9d
Jakob Schiotz <schiotz@fysik.dtu.dk>
2025-01-05 00:00:29 +0100

    Stylistic improvements suggested by @yuzie007.

--- ase/test/md/test_langevin_offbyone.py
+++ ase/test/md/test_langevin_offbyone.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pytest
 
-# matplotlib only imported in debugging mode (in function test_nvt)
-# import matplotlib.pyplot as plt
 from ase.build import bulk
 from ase.md.langevin import Langevin
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution, Stationary
@@ -17,32 +15,28 @@ def make_atoms(T, rng, asap3):
     return atoms
 
 
-class MeasureEnergy:
+class EnergyMeasurer:
     def __init__(self, atoms):
         self.atoms = atoms
         self.energies = []
         self.temperatures = []
 
     def __call__(self):
-        e = self.atoms.get_potential_energy() + self.atoms.get_kinetic_energy()
-        T = self.atoms.get_temperature()
-        self.energies.append(e)
-        self.temperatures.append(T)
+        energy = self.atoms.get_potential_energy() \
+            + self.atoms.get_kinetic_energy()
+        temperature = self.atoms.get_temperature()
+        self.energies.append(energy)
+        self.temperatures.append(temperature)
 
 
-def tempcurve(t, A, tau, B):
-    'Temperature curve to fit to'
-    return A * np.exp(-t / tau) + B
-
-
-def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None, intval=5):
+def run_nvt(atoms, nsteps, dt, expected_T, dynmaker, rng=None, intval=5):
     '''Run NVT dynamics, testing the behaviour.
 
     Parameters:
     atoms:      The atoms object.
     nsteps:     Length of simulation.
     dt:         Time step.
-    T0:         Expected temperature
+    expected_T: Expected temperature
     dynmaker:   Function making the dynamics object.
     rng:        Random number generator.
     intval:     Interval for taking data.
@@ -54,14 +48,14 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None, intval=5):
     # dynamics, as we have a solid, where the relaxation time will be twice
     # that of the ideal gas, since the same amount of potential and kinetic
     # energy needs to be added to the system.
-    dyn = dynmaker(atoms, T0, dt, tau / 2, rng=rng, logint=250)
-    measure = MeasureEnergy(atoms)
+    dyn = dynmaker(atoms, expected_T, dt, tau / 2, rng=rng, loginterval=250)
+    measure = EnergyMeasurer(atoms)
     dyn.attach(measure, interval=1)
     dyn.run(nsteps)
 
     # Run again while taking data
-    dyn = dynmaker(atoms, T0, dt, tau, rng=rng, logint=2500)
-    measure = MeasureEnergy(atoms)
+    dyn = dynmaker(atoms, expected_T, dt, tau, rng=rng, loginterval=2500)
+    measure = EnergyMeasurer(atoms)
     dyn.attach(measure, interval=intval)
     com_before = atoms.get_center_of_mass()
     dyn.run(nsteps * 20)
@@ -74,7 +68,7 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None, intval=5):
     stdE = np.std(energies)
     avgT = np.mean(temperatures2)
     # Expected energy fluctuation: sqrt(k_B T^2 3 N k_B) = k_B * T * sqrt(3 * N)
-    expected = kB * T0 * np.sqrt(3 * len(atoms))
+    expected = kB * expected_T * np.sqrt(3 * len(atoms))
 
     # Output results
     print(f'Observed energy fluctuation: {stdE:.2f} eV')
@@ -83,23 +77,31 @@ def run_nvt(atoms, nsteps, dt, T0, dynmaker, rng=None, intval=5):
     assert np.abs(stdE - expected) < 0.25 * expected, 'Energy fluctuations'
 
     # Temperature error: We should be able to detect a error of 1/N_atoms
-    maxtemperr = 2 / 3 * 3 / atoms.get_number_of_degrees_of_freedom()
+    # The factor .67 is arbitrary, smaller than 1.0 so we consistently
+    # detect errors, but not so small that we get false positives.
+    maxtemperr = 0.67 * 1 / len(atoms)
     # ... but not if we don't have good statistics.
     print(f'Observed average temperature:  {avgT:.2f} K'
-              + f'   (expected {T0:.2f} K)')
-    print(f'Error: {(avgT / T0 - 1) * 100:.1f}%  '
+              + f'   (expected {expected_T:.2f} K)')
+    print(f'Error: {(avgT / expected_T - 1) * 100:.1f}%  '
               + f'(max: {maxtemperr * 100:.1f}%)')
-    assert np.abs(avgT - T0) < T0 * maxtemperr, 'Temperature'
+    assert np.abs(avgT - expected_T) < expected_T * maxtemperr, 'Temperature'
 
     print('Center of mass before:', com_before)
     print('Center of mass after: ', com_after)
 
 
-def lgvdynmaker(atoms, T0, dt, tau, rng, logint):
+def make_langevin(atoms, desired_T, dt, tau, rng, loginterval):
     # tau is the energy relaxation time.  The velocity relaxation time
     # should be the double.
-    return Langevin(atoms, dt, temperature_K=T0, friction=1 / (2 * tau),
-                        logfile='-', loginterval=logint, rng=rng)
+    return Langevin(atoms,
+                    dt,
+                    temperature_K=desired_T,
+                    friction=1 / (2 * tau),
+                    logfile='-',
+                    loginterval=loginterval,
+                    rng=rng,
+    )
 
 
 @pytest.mark.slow()
@@ -110,5 +112,5 @@ def test_langevin_offbyone(asap3):
     rng = np.random.default_rng(2718281828459045)
 
     atoms = make_atoms(T0 / 10, rng, asap3)
-    run_nvt(atoms, nsteps, dt, T0, dynmaker=lgvdynmaker,
+    run_nvt(atoms, nsteps, dt, T0, dynmaker=make_langevin,
             rng=rng)
