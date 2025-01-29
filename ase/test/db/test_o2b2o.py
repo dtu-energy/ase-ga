89a3e9f4111c67029a7672609c70f6ddfba7dbe4
Jens Jørgen Mortensen <jj@smoerhul.dk>
2025-01-29 14:29:06 +0100

    Test empty array

--- ase/test/db/test_o2b2o.py
+++ ase/test/db/test_o2b2o.py
@@ -7,7 +7,7 @@ from ase.db.core import bytes_to_object, object_to_bytes
 def test_o2b2o():
     for o1 in [1.0,
                {'a': np.zeros((2, 2), np.float32),
-                'b': np.zeros((1, 2), int)},
+                'b': np.zeros((0, 2), int)},
                ['a', 42, True, None, np.nan, np.inf, 1j],
                Cell(np.eye(3)),
                {'a': {'b': {'c': np.ones(3)}}}]:
