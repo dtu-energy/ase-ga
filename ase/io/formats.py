24934132bccec88220762596347b63c3ca7b7ec7
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-03-31 15:15:10 +0200

    do not represent this as an ioformat, because it is the job of ase-db-backends to provide an ioformat as a plugin

--- ase/io/formats.py
+++ ase/io/formats.py
@@ -361,7 +361,8 @@ F('aims', 'FHI-aims geometry file', '1S', ext='in')
 F('aims-output', 'FHI-aims output', '+S',
   module='aims', magic=b'*Invoking FHI-aims ...')
 F('bundletrajectory', 'ASE bundle trajectory', '+S')
-F('aselmdb', 'ASE LMDB format', '+F')
+# XXX: Define plugin in ase db backends package:
+# F('aselmdb', 'ASE LMDB format', '+F')
 F('castep-castep', 'CASTEP output file', '+F',
   module='castep', ext='castep')
 F('castep-cell', 'CASTEP geom file', '1F',
