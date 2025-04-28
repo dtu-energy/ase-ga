038ec0c793e516cdd084d475ae9d7f02449a7c9f
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-04-28 16:40:58 +0200

    include basic columns, scrollbar

--- ase/gui/atomseditor.py
+++ ase/gui/atomseditor.py
@@ -8,5 +8,26 @@ class AtomsEditor:
 
         win = ui.Window(_('Edit atoms'), wmtype='utility')
 
-        self.gui = gui
-        self.win = win
+        tree = ui.ttk.Treeview(win.win, selectmode='browse')
+        tree.pack(side='left', fill='y')
+        bar = ui.ttk.Scrollbar(win.win, orient='vertical', command=tree.yview)
+        tree.configure(yscrollcommand=bar.set)
+        bar.pack(side='right', fill='y')
+
+        columns = ['symbol', *'xyz']
+        tree['columns'] = columns
+
+        tree.column('#0', width=40)
+        tree.heading('#0', text=_('id'))
+
+        tree.column('symbol', width=60)
+        tree.heading('symbol', text=_('symbol'))
+
+        for name in 'xyz':
+            tree.column(name, width=80)
+            tree.heading(name, text=name)
+
+        atoms = gui.atoms
+        for i in range(len(atoms)):
+            values = [atoms.symbols[i], *atoms.positions[i]]
+            tree.insert('', 'end', text=i, values=values)
