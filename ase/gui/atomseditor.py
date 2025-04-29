70238af921a3c400a67ccbcab2f00f58dc149b64
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-04-29 14:48:34 +0200

    towards updating atoms in atoms editor

--- ase/gui/atomseditor.py
+++ ase/gui/atomseditor.py
@@ -27,28 +27,58 @@ class AtomsEditor:
         tree.configure(yscrollcommand=bar.set)
         bar.pack(side='right', fill='y')
 
-        columns = ['symbol', *'xyz']
-        tree['columns'] = columns
-
         tree.column('#0', width=40)
         tree.heading('#0', text=_('id'))
 
-        tree.column('symbol', width=60)
-        tree.heading('symbol', text=_('symbol'))
+        def get_symbol(atoms, i):
+            return atoms.symbols[i]
 
-        for name in 'xyz':
-            tree.column(name, width=80)
-            tree.heading(name, text=name)
+        def set_symbol(atoms, i, value):
+            from ase.data import atomic_numbers
 
-        atoms = gui.atoms
-        for i in range(len(atoms)):
-            values = [atoms.symbols[i], *atoms.positions[i]]
-            tree.insert('', 'end', text=i, values=values)
+            if value not in atomic_numbers:
+                pass  # Display error?
+            atoms.symbols[i] = value
+
+        self.gui = gui
+        self.tree = tree
+        self.columns = []
+
+        self.add_column(
+            'symbol',
+            _('symbol'),
+            60,
+            lambda atoms, i: atoms.symbols[i],
+            set_symbol,
+        )
+
+        self.update_table_from_atoms()
 
         tree.bind('<Return>', self.pressed_return)
         tree.bind('<Double-1>', self.doubleclick)
 
-        self.tree = tree
+    @property
+    def atoms(self):
+        return self.gui.atoms
+
+    def update_table_from_atoms(self):
+        for i in range(len(self.atoms)):
+            values = self.get_row_values(i)
+            self.tree.insert('', 'end', text=i, values=values)
+
+    def get_row_values(self, i):
+        return [column.getvalue(self.gui.atoms, i) for column in self.columns]
+
+    def add_column(self, name, heading, width, getvalue, setvalue):
+        column = Column(name, heading, width, getvalue, setvalue)
+
+        names = [column.name for column in self.columns]
+        names.append(name)
+
+        self.tree['columns'] = names
+        self.tree.column(name, width=width)
+        self.tree.heading(name, text=heading)
+        self.columns.append(column)
 
     def pressed_return(self, event):
         print(event)
@@ -56,25 +86,25 @@ class AtomsEditor:
     def doubleclick(self, event):
         print(event)
 
-        # what row and column was clicked on
         row_id = self.tree.identify_row(event.y)
         column_id = self.tree.identify_column(event.x)
-        print('click', row_id, column_id)
 
         assert column_id[0] == '#'
-        column = int(column_id[1:])
+        assert row_id.startswith('I')
 
+        column_no = int(column_id[1:]) - 1
+        if column_no == -1:
+            return  # This is the ID column.
+        row_no = int(row_id[1:].rstrip('0')) - 1
+
+        assert 0 <= column_no < len(self.columns)
+        assert 0 <= row_no < len(self.atoms)
 
         x, y, width, height = self.tree.bbox(row_id, column_id)
 
         item = self.tree.item(row_id, 'values')
-        print(item)
-        if column == 0:
-            return
 
-        print('column', column)
-        content = item[column - 1]
-        print(content)
+        content = item[column_no]
 
         entry = ui.ttk.Entry(self.tree)
         entry.insert(0, content)
@@ -83,11 +113,15 @@ class AtomsEditor:
 
         def apply_change(event):
             print('apply', event, entry, entry.get())
+            column = self.columns[column_no]
+            print('Column', column)
+            value = entry.get()
+            column.setvalue(self.atoms, row_no, value)
+            self.tree.set(row_id, column_id, value=value)
+            self.gui.set_frame()
             entry.destroy()
 
         entry.bind('<Return>', apply_change)
         entry.bind('<Escape>', lambda *args: entry.destroy())
 
-        # entry.place(row_id, column - 1, 'hello')
         entry.place(x=x, y=y, width=width, height=height)
-
