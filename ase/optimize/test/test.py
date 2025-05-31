a0ee869441cc58069549839fb99ec6eef8ce0992
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-05-31 19:04:17 +0200

    Check types in `ase.optimize.test`

--- ase/optimize/test/test.py
+++ ase/optimize/test/test.py
@@ -4,11 +4,13 @@ import argparse
 import traceback
 from math import pi
 from time import time
+from typing import Union
 
 import numpy as np
 
 import ase.db
 import ase.optimize
+from ase import Atoms
 from ase.calculators.emt import EMT
 from ase.io import Trajectory
 
@@ -32,16 +34,21 @@ def get_optimizer(name):
 class Wrapper:
     """Atoms-object wrapper that can count number of moves."""
 
-    def __init__(self, atoms, gridspacing=0.2, eggbox=0.0):
-        # types: (Atoms, float, float) -> None
+    def __init__(
+        self,
+        atoms: Atoms,
+        gridspacing: float = 0.2,
+        eggbox: float = 0.0,
+    ) -> None:
         self.t0 = time()
         self.texcl = 0.0
         self.nsteps = 0
         self.atoms = atoms
         self.ready = False
-        self.pos = None  # type: np.ndarray
+        self.pos: Union[np.ndarray, None] = None
         self.eggbox = eggbox
 
+        self.x = None
         if eggbox:
             # Find small unit cell for grid-points
             h = []
@@ -50,8 +57,6 @@ class Wrapper:
                 n = int(L / gridspacing)
                 h.append(axis / n)
             self.x = np.linalg.inv(h)
-        else:
-            self.x = None
 
     def get_potential_energy(self, force_consistent=False):
         t1 = time()
