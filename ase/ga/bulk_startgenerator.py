6dba059ad93e31caaa8c6aa65cd291a46b0af924
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-07-16 00:07:24 +0200

    remove old placeholder modules

--- ase/ga/bulk_startgenerator.py
+++
@@ -1,7 +0,0 @@
-# fmt: off
-
-raise ImportError(
-    'The ase.ga.bulk_startgenerator module has been deprecated. '
-    'The same functionality is now provided by the '
-    'ase.ga.startgenerator module. Please consult its documentation '
-    'to verify how to e.g. initialize a StartGenerator object.')
--- ase/ga/bulk_utilities.py
+++
@@ -1,6 +0,0 @@
-# fmt: off
-
-raise ImportError(
-    'The ase.ga.bulk_utilities module has been deprecated. '
-    'The same functionality is now provided by the '
-    'ase.ga.utilities module.')
