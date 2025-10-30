41c9cee887a03da76402208d2e377e574a952dea
Steen Lysgaard <stly@dtu.dk>
2025-10-30 14:54:14 +0100

    get mol role from ase

--- doc/conf.py
+++ doc/conf.py
@@ -2,9 +2,10 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path('..', 'src').resolve()))
-
+sys.path.append('.')
 
 extensions = [
+    'ext',  # mol role from ase
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',  # one page per object
     'sphinx.ext.doctest',
@@ -13,15 +14,13 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.imgconverter',
 ]
 
 extlinks = {
     'doi': ('https://doi.org/%s', 'doi: %s'),
     'arxiv': ('https://arxiv.org/abs/%s', 'arXiv: %s'),
-    # 'mr': ('https://gitlab.com/ase/ase/-/merge_requests/%s', 'MR: !%s'),
-    # 'issue': ('https://gitlab.com/ase/ase/-/issues/%s', 'issue: #%s'),
 }
+
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'ASE_GA'
@@ -31,16 +30,6 @@ project = 'ASE_GA'
 # default_role = 'math'
 pygments_style = 'sphinx'
 autoclass_content = 'both'
-# modindex_common_prefix = ['ase.']
-# nitpick_ignore = [
-#     ('envvar', 'VASP_PP_PATH'),
-#     ('envvar', 'ASE_ABC_COMMAND'),
-#     ('envvar', 'LAMMPS_COMMAND'),
-#     ('envvar', 'ASE_NWCHEM_COMMAND'),
-#     ('envvar', 'SIESTA_COMMAND'),
-#     ('envvar', 'SIESTA_PP_PATH'),
-#     ('envvar', 'VASP_SCRIPT'),
-# ]
 
 html_theme = 'sphinx_book_theme'
 # html_logo = 'static/ase256.png'
@@ -49,19 +38,9 @@ html_static_path = ['static']
 html_last_updated_fmt = '%a, %d %b %Y %H:%M:%S'
 
 html_theme_options = {
-    # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/header-links.html
-    # 'gitlab_url': 'https://gitlab.com/ase/ase',
-    # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/indices.html
+    'github_url': 'https://github.com/dtu-energy/ase-ga',
     'primary_sidebar_end': ['indices.html'],
 }
-
-# latex_elements = {'papersize': 'a4paper'}
-# latex_show_urls = 'inline'
-# latex_show_pagerefs = True
-# latex_engine = 'xelatex'
-# latex_documents = [
-#     ('index', 'ASE.tex', 'ASE', 'ASE-developers', 'howto', not True)
-# ]
 
 # Don't sort members alphabetically
 autodoc_member_order = 'bysource'
@@ -71,7 +50,7 @@ autodoc_preserve_defaults = True
 
 intersphinx_mapping = {
     'ase': ('https://ase-lib.org/', None),
-    'python': ('https://docs.python.org/3.10', None),
+    'python': ('https://docs.python.org/3', None),
 }
 
 # # Avoid GUI windows during doctest:
@@ -82,5 +61,3 @@ intersphinx_mapping = {
 # visualize.view = lambda atoms: None
 # Atoms.edit = lambda self: None
 # """
-
-# autodoc_mock_imports = ['kimpy']
